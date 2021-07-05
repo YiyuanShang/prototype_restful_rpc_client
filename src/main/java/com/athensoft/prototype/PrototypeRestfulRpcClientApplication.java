efa9e9bc939bd3cef36fa80af9e8a46f19b3b0a9
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-07-05 18:31:37 -0400

    init project

---
+++ src/main/java/com/athensoft/prototype/PrototypeRestfulRpcClientApplication.java
@@ -0,0 +1,13 @@
+package com.athensoft.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeRestfulRpcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeRestfulRpcClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athensoft/prototype/PrototypeRestfulRpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athensoft.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeRestfulRpcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
